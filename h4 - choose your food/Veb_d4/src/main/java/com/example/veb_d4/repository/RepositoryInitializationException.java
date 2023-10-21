a4719f8bbacc3da19ea3ac74f8004b37cdde2fee
JovanovicLela <96892264+JovanovicLela@users.noreply.github.com>
2023-10-21 13:15:43 +0200

    Delete h4 - choose your food/Veb_d4 directory

--- h4 - choose your food/Veb_d4/src/main/java/com/example/veb_d4/repository/RepositoryInitializationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.veb_d4.repository;
-
-public class RepositoryInitializationException extends Exception {
-    public RepositoryInitializationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
