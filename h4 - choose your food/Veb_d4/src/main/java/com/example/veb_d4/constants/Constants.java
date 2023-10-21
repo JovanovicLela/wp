801d8a7fbe437f61a69cb9c10228a81ea7607a51
JovanovicLela <96892264+JovanovicLela@users.noreply.github.com>
2023-10-21 13:14:56 +0200

    Add files via upload

---
+++ h4 - choose your food/Veb_d4/src/main/java/com/example/veb_d4/constants/Constants.java
@@ -0,0 +1,19 @@
+package com.example.veb_d4.constants;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Constants {
+    public static final String PATH = "C:\\Users\\lex59\\OneDrive\\Desktop\\Veb\\Domaci\\h4 - choose your food\\Veb_d4\\src\\main\\resources\\";
+    public static final String PASSWORD_FILE = "password.txt";
+
+    public static final String MONDAY_FILE = "C:\\Users\\lex59\\OneDrive\\Desktop\\Veb\\Domaci\\h4 - choose your food\\Veb_d4\\src\\main\\resources\\monday.txt";
+    public static final String TUESDAY_FILE = "C:\\Users\\lex59\\OneDrive\\Desktop\\Veb\\Domaci\\h4 - choose your food\\Veb_d4\\src\\main\\resources\\tuesday.txt";
+    public static final String WEDNESDAY_FILE = "C:\\Users\\lex59\\OneDrive\\Desktop\\Veb\\Domaci\\h4 - choose your food\\Veb_d4\\src\\main\\resources\\wednesday.txt";
+    public static final String THURSDAY_FILE = "C:\\Users\\lex59\\OneDrive\\Desktop\\Veb\\Domaci\\h4 - choose your food\\Veb_d4\\src\\main\\resources\\thursday.txt";
+    public static final String FRIDAY_FILE = "C:\\Users\\lex59\\OneDrive\\Desktop\\Veb\\Domaci\\h4 - choose your food\\Veb_d4\\src\\main\\resources\\friday.txt";
+
+    public static final List<String> days =
+            Collections.unmodifiableList(Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"));
+}
