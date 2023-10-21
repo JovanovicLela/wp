e00af85e61814ee308952011c3bd4976135afcce
JovanovicLela <96892264+JovanovicLela@users.noreply.github.com>
2023-10-21 13:13:09 +0200

    Add files via upload

---
+++ h4 - choose your food/Veb_d4/src/main/java/com/example/veb_d4/repository/RepositoryInitializationException.java
@@ -0,0 +1,7 @@
+package com.example.veb_d4.repository;
+
+public class RepositoryInitializationException extends Exception {
+    public RepositoryInitializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
